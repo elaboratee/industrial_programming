6b184c14ce95d73183c0b8747d129bd982816982
Dmitry <moskvitin.05@mail.ru>
2024-03-07 22:58:23 +0300

    add Accessories

--- ЛАБ_4/LAB_4/src/task_2/accessories/Accessory.java
+++ ЛАБ_4/LAB_4/src/task_2/accessories/Accessory.java
@@ -1,5 +1,44 @@
 package task_2.accessories;
 
-public abstract class Accessory {
+import java.util.Objects;
 
+public abstract class Accessory {
+    protected int price;
+    protected String color;
+    protected String material;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Accessory accessory)) return false;
+        return price == accessory.price &&
+                Objects.equals(color, accessory.color) &&
+                Objects.equals(material, accessory.material);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price, color, material);
+    }
+
+    @Override
+    public String toString() {
+        return "Accessory{" +
+                "price=" + price +
+                ", color='" + color + '\'' +
+                ", material='" + material + '\'' +
+                '}';
+    }
 }
